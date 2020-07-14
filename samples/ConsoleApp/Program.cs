3e10cb903d5ddec6cca613908a3fb122d2232345
Aki Pehkonen <aki.pehkonen@nortal.com>
2020-07-14 14:46:48 +0300

    initial

---
+++ samples/ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter("Microsoft", LogLevel.Warning)
+                    .AddFilter("System", LogLevel.Warning)
+                    .AddFilter("Google.Cloud.Extensions.Configuration.SecretManager.SecretManagerConfigurationProvider", LogLevel.Debug)
+                    .AddConsole();
+            });
+
+            IConfiguration config = new ConfigurationBuilder()
+                .AddSecretManager(c => {
+                        c.ProjectName = Environment.GetEnvironmentVariable("GOOGLE_PROJECT_ID");
+                        c.LoggerFactory = loggerFactory;
+                    }
+                )
+                .Build();
+
+            Console.Write($"Param1 value is {config["param1"]}");
+        }
+    }
+}
